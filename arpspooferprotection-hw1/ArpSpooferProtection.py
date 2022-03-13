3eed22bb4a18dc08d39996f509cba438bc46e6ff
didi <ymarashe@g.jct.ac.il>
2022-03-13 17:06:24 +0200

    add INDICATOR 3

--- arpspooferprotection-hw1/ArpSpooferProtection.py
+++ arpspooferprotection-hw1/ArpSpooferProtection.py
@@ -1,17 +1,20 @@
+from urllib import response
+from numpy import broadcast
 from scapy.all import *
+import re
 
+
+mac_address_regex = re.compile(r'([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})')
+broadcast_mac_address = 'ff:ff:ff:ff:ff:ff'
 ARPisat = 2
 
 indicators = []
 
 class ArpSpoofDetectSession(DefaultSession):
-
     def on_packet_received(self, pkt):
         """
         for every arp packet that comes through, check the following
         - ping the host and see if there is a response
-        - get ARP table and look to see duplicate entries with different IPs
-        - DIDI THINK OF SOMETHING
         """
         # INDICATOR 1: Host machine of is_at pkt not responding to ping req.
         if pkt[ARP].op == ARPisat:
@@ -21,8 +24,41 @@ class ArpSpoofDetectSession(DefaultSession):
                 indicators.append("host not replying to ping")
                 return
 
-        # INDICATOR 2: Arp table contains duplicate MACs for different IPs
-        # INDICATOR 3: DIDI YA SHARMIT
+    # INDICATOR 2: Arp table contains duplicate MACs for different IPs
+    def arp_table_function():
+        """
+        - get ARP table and look to see duplicate entries with different IPs
+        """
+        arp_table = open("/proc/net/arp", "r").read()
+        arp_table = arp_table.split('\n')
+        MAC_addresses = []
+        for arpLine in arp_table:
+            if arpLine == '':
+                continue
+            arpLine = arpLine.split(" ")
+            if re.match(mac_address_regex,arpLine[25]):
+                MAC_addresses.append(arpLine[25])
+            for i in MAC_addresses:
+                if MAC_addresses.count(i) > 1:
+                    indicators.append("warning: there is 2 same mac address in the arp table with 2 different ip address")
+                    return     
+
+    def duplicatesAnswars(self, pkt):
+        """
+        - check if there is a duplicate answer
+        """
+        if pkt[ARP].op == ARPisat:
+            #send arp who is request to the pkt and if there is 2 answers, it is a duplicate answer
+            arp_who_is = Ether(dst=broadcast_mac_address)/ARP(ARP.who_has,pdst=pkt[ARP].pscr)
+            response = List(sr(arp_who_is, timeout=2))
+            if len(response) > 1:
+                for i in (len(response)-1):
+                    if response[i][1][ARP].hwsrc != response[i+1][1][ARP].hwsrc:
+                        indicators.append("warning: there is a duplicate answer")
+                        return
+
+    
+    # INDICATOR 3: DIDI YA SHARMIT
 
 sniff(lfilter= lambda pkt: (ARP in pkt and pkt[ARP].hwsrc != (Ether())[Ether].src),session=ArpSpoofDetectSession, stop_filter=lambda x: len(indicators) >= 2)
 if len(indicators) >= 2:
@@ -30,42 +66,7 @@ if len(indicators) >= 2:
     for ind in indicators:
         print(ind)
 
-# import os
-# import re
-# import time
-
-# mac_address_regex = re.compile(r'([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})')
 
 
-# def check_mac_address(mac_address):
-#     if re.match(mac_address_regex, mac_address):
-#         return True
-#     else:
-#         return False
-
-# def arp_table_function():
-#     arp_table = os.popen("arp -a").read()
     
-#     arp_table = arp_table.split('\n')
-    
-#     MAC_addresses = []
-#     for arpLine in arp_table:
-#         if arpLine == '':
-#             continue
-#         arpLine = arpLine.split(" ")
-#         if check_mac_address(arpLine[3]):
-#             MAC_addresses.append(arpLine[3])
-
-#     return MAC_addresses
         
-# def find_a_match():
-#     MAC_addresses= arp_table_function()
-#     #check if there is 2 same mac address in the arp table with 2 different ip address
-#     for i in MAC_addresses:
-#         if MAC_addresses.count(i) > 1:
-#             print("warning: there is 2 same mac address in the arp table with 2 different ip address")
-
-# if __name__ == "__main__":
-#     while True:
-#         find_a_match()
-#         time.sleep(15)
