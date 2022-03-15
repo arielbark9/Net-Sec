1b3f79585e883d91e66dac52a39f97338917adcd
arielbark9 <arielbark9@gmail.com>
2022-03-15 12:37:43 -0400

    hw1 finished

--- arpspooferprotection-hw1/ArpSpooferProtection.py
+++ arpspooferprotection-hw1/ArpSpooferProtection.py
@@ -2,6 +2,7 @@ from scapy.all import *
 import re
 from scapy.layers.l2 import Ether, ARP
 from termcolor import colored
+import platform
 
 mac_address_regex = re.compile(r'([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})')
 broadcast_mac_address = 'ff:ff:ff:ff:ff:ff'
@@ -35,7 +36,11 @@ def arp_table_contains_duplicates():
     """
     - get ARP table and look for duplicate MAC entries with different IPs
     """
-    arp_table = open("/proc/net/arp", "r").read()
+    if platform.system() == "Linux":
+        arp_table = open("/proc/net/arp", "r").read()
+    elif platform.system() in ["Darwin", "Windows"]:
+        arp_table = os.popen("arp -a").read()
+
     arp_table = arp_table.split('\n')
     mac_addresses = []
     for arp_line in arp_table:
@@ -65,7 +70,11 @@ def responds_to_ping_request(pkt):
 
 
 def print_state():
-    os.system("clear")
+    if platform.system() in ["Linux", "Darwin"]:
+        os.system("clear")
+    elif platform.system() == "Windows":
+        os.system("cls")
+
     print("The Following indicators are being reported:\n"
           f"Host of Arp is_at packet not responding to a Ping request:  {colored(indicators['I1'], 'red')}\n"
           f"Receiving multiple responses per one Arp who_has request:   {colored(indicators['I2'], 'red')}\n"
@@ -96,7 +105,7 @@ class ArpSpoofDetectSession(DefaultSession):
 
 def main():
     print_state()
-    sniff(lfilter=lambda pkt: (ARP in pkt and pkt[ARP].hwsrc != (Ether())[Ether].src),
+    sniff(lfilter=lambda pkt: (ARP in pkt and pkt[ARP].hwsrc != (Ether())[Ether].src and pkt[ARP].op == ARPisat),
           session=ArpSpoofDetectSession)
 
 
